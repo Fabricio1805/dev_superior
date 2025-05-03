81f0c957781a4a8eb0e3c95918256ac146bd9ffd
Fabricio1805 <fabricio.ss09@outlook.com>
2025-05-03 16:58:38 -0300

    frist commit

---
+++ src/main/java/com/devsuperior/componenteinjecaodependencia/ComponenteInjecaoDependenciaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.componenteinjecaodependencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponenteInjecaoDependenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponenteInjecaoDependenciaApplication.class, args);
+	}
+
+}
